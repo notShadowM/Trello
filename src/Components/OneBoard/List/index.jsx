243a62844e75b9613e2e0af51243b02e616dcfc4
notShadowM <maanabahin2001@gmail.com>
2022-03-03 14:20:18 +0200

    render issues

--- src/Components/OneBoard/List/index.jsx
+++ src/Components/OneBoard/List/index.jsx
@@ -2,7 +2,7 @@ import { CloseOutlined, PlusOutlined } from '@ant-design/icons/lib/icons';
 import { Button, Input } from 'antd';
 import { toJS } from 'mobx';
 import { observer } from 'mobx-react';
-import React, { useRef, useState } from 'react';
+import React, { useMemo, useRef, useState } from 'react';
 import { Container, Draggable } from 'react-smooth-dnd';
 import { useStore } from '../../../app/stores';
 import Card from '../Card';
@@ -11,6 +11,7 @@ function List({ list, boardId }) {
   const addInput = useRef();
   const [showAdd, setShowAdd] = useState(false);
   const [text, setText] = useState('');
+  const [dropped, setDropped] = useState(false);
   const { BoardsStore } = useStore();
   const { cards } = BoardsStore.boards.find((e) => e.id === boardId).list.find((listItem) => listItem.id === list.id);
 
@@ -22,6 +23,20 @@ function List({ list, boardId }) {
     }
   };
 
+  const listForm = useMemo(() => (
+    <form onSubmit={(e) => { e.preventDefault(); addCard(); }} className={`addCard ${showAdd ? 'showAddCard' : ''}`}>
+      <Input ref={addInput} placeholder="Enter list title..." className="cardInput" value={text} onChange={(e) => setText(e.target.value)} />
+      <div className="addListSecRow">
+        <Button type="primary" onClick={() => addCard()}>Add list</Button>
+        <CloseOutlined style={{ fontSize: '20px' }} onClick={() => { setShowAdd(false); setText(''); }} />
+      </div>
+    </form>
+  ), [showAdd, text]);
+
+  const listCards = useMemo(
+    () => cards?.map((card, index) => <Draggable key={index}><Card card={card} listId={list.id} boardId={boardId} /></Draggable>),
+    [cards, cards.length, dropped],
+  );
   return (
     <div className="listCard">
       <div>
@@ -30,7 +45,12 @@ function List({ list, boardId }) {
         <div className="cards">
           <Container
             groupName="col"
-            onDrop={(dragResult) => BoardsStore.switchCards(dragResult, list.id, boardId)}
+            onDrop={(dragResult) => {
+              if (dragResult.removedIndex !== dragResult.addedIndex) {
+                BoardsStore.switchCards(dragResult, list.id, boardId);
+                setDropped((prev) => !prev);
+              }
+            }}
             getChildPayload={(index) => ({ card: toJS(BoardsStore.getCard(index, list.id, boardId)), listId: list.id })}
             dragClass="card-ghost"
             dropClass="card-ghost-drop"
@@ -41,7 +61,7 @@ function List({ list, boardId }) {
             }}
             dropPlaceholderAnimationDuration={200}
           >
-            {cards?.map((card, index) => <Draggable key={index}><Card card={card} listId={list.id} boardId={boardId} /></Draggable>)}
+            {listCards}
           </Container>
         </div>
         <div className={`addCardRow ${showAdd ? 'hidden' : ''}`} onClick={(e) => { setShowAdd(true); addInput.current.focus(); }}>
@@ -51,15 +71,9 @@ function List({ list, boardId }) {
         </div>
       </div>
 
-      <form onSubmit={(e) => { e.preventDefault(); addCard(); }} className={`addCard ${showAdd ? 'showAddCard' : ''}`}>
-        <Input ref={addInput} placeholder="Enter list title..." className="cardInput" value={text} onChange={(e) => setText(e.target.value)} />
-        <div className="addListSecRow">
-          <Button type="primary" onClick={() => addCard()}>Add list</Button>
-          <CloseOutlined style={{ fontSize: '20px' }} onClick={() => { setShowAdd(false); setText(''); }} />
-        </div>
-      </form>
+      {listForm}
     </div>
   );
 }
 
-export default React.memo(observer(List));
+export default observer(List);
--- src/Components/OneBoard/index.jsx
+++ src/Components/OneBoard/index.jsx
@@ -1,5 +1,7 @@
 import { toJS } from 'mobx';
-import React, { useEffect, useRef, useState } from 'react';
+import React, {
+  useEffect, useRef, useState, useMemo,
+} from 'react';
 import { useLocation } from 'react-router-dom';
 import Helmet from 'react-helmet';
 import { Input, Button } from 'antd';
@@ -15,6 +17,7 @@ function OneBoard() {
   const { pathname } = useLocation();
   const [showAdd, setShowAdd] = useState(false);
   const [text, setText] = useState('');
+  const [dropped, setDropped] = useState(false);
   const boardId = pathname.substring(1);
   const { BoardsStore } = useStore();
   const board = BoardsStore.boards.find((e) => e.id === boardId);
@@ -27,14 +30,41 @@ function OneBoard() {
     }
   };
 
+  const boardListsItems = useMemo(
+    () => lists?.map((listItem, index) => <Draggable key={index}><List boardId={boardId} list={listItem} /></Draggable>),
+    [dropped, lists.length],
+  );
+
+  const addListBlock = useMemo(() => (
+    <div className="addListBlock">
+      <div className="addListThumbnail " onClick={() => { setShowAdd(true); addInput.current.focus(); }}>
+        <PlusOutlined style={{ color: '#fff', fontSize: '15px' }} />
+        {' '}
+        Add a list
+      </div>
+      <form onSubmit={(e) => { e.preventDefault(); addList(); }} className={`addList ${showAdd ? 'showAddList' : ''}`}>
+        <Input ref={addInput} placeholder="Enter list title..." className="listInput" value={text} onChange={(e) => setText(e.target.value)} />
+        <div className="addListSecRow">
+          <Button type="primary" onClick={() => addList()}>Add list</Button>
+          <CloseOutlined style={{ fontSize: '20px' }} onClick={() => { setShowAdd(false); setText(''); }} />
+        </div>
+      </form>
+    </div>
+  ), [showAdd, text]);
+
   return (
-    <>
+    <div className="test">
       <Helmet bodyAttributes={{ style: `background-color : ${board.background}` }} />
       <Container
         orientation="horizontal"
-        onDrop={(e) => BoardsStore.switchLists(e, boardId)}
+        onDrop={(dragResult) => {
+          if (dragResult.removedIndex !== dragResult.addedIndex) {
+            BoardsStore.switchLists(dragResult, boardId);
+            setDropped((prev) => !prev);
+          }
+        }}
         style={{
-          display: 'flex', width: '96%', margin: '15px auto', gap: '1%', alignItems: 'flex-start',
+          display: 'flex', margin: '15px 0', gap: '8px', alignItems: 'flex-start',
         }}
         dragClass="card-ghost"
         dropClass="card-ghost-drop"
@@ -45,24 +75,11 @@ function OneBoard() {
         }}
         dropPlaceholderAnimationDuration={200}
       >
-        {lists?.map((listItem, index) => <Draggable key={index}><List boardId={boardId} list={listItem} /></Draggable>)}
+        {boardListsItems}
 
-        <div className="addListBlock">
-          <div className="addListThumbnail " onClick={() => { setShowAdd(true); addInput.current.focus(); }}>
-            <PlusOutlined style={{ color: '#fff', fontSize: '15px' }} />
-            {' '}
-            Add a list
-          </div>
-          <form onSubmit={(e) => { e.preventDefault(); addList(); }} className={`addList ${showAdd ? 'showAddList' : ''}`}>
-            <Input ref={addInput} placeholder="Enter list title..." className="listInput" value={text} onChange={(e) => setText(e.target.value)} />
-            <div className="addListSecRow">
-              <Button type="primary" onClick={() => addList()}>Add list</Button>
-              <CloseOutlined style={{ fontSize: '20px' }} onClick={() => { setShowAdd(false); setText(''); }} />
-            </div>
-          </form>
-        </div>
       </Container>
-    </>
+      {addListBlock}
+    </div>
   );
 }
 
